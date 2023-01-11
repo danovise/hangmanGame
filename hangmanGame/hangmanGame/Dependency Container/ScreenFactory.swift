63cbb9bd9c38089301c7e89ec10f9f7d5d1c76ef
danovise <novik301199@gmail.com>
2023-01-11 19:04:49 +0300

    build(app): added alert extension

--- hangmanGame/hangmanGame/Dependency Container/ScreenFactory.swift
+++ hangmanGame/hangmanGame/Dependency Container/ScreenFactory.swift
@@ -46,5 +46,4 @@ class ScreenFactoryImpl: ScreenFactory {
         
         return howToPlayVC
     }
-    
 }
--- hangmanGame/hangmanGame/Extensions/UIViewController+alert.swift
+++ hangmanGame/hangmanGame/Extensions/UIViewController+alert.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+extension UIViewController {
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alertController, animated: true)
+    }
+}
